a3562907da53151e4c7058bc0fd82486aa6b447e
shimuzinho <pedro.moraes2@estudante.ifms.edu.br>
2024-03-08 14:41:53 -0400

    Criação do Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import Header from "./Components/Header"
 import Footer from "./Components/Footer";
 
 export default function App() {
   return(
     <main>
+      <Header/>
       <Footer/>
     </main>
   );
