5602cfcf6f5bf838069048a78487b9135dbf283a
shimuzinho <pedro.moraes2@estudante.ifms.edu.br>
2024-03-13 03:12:27 -0300

    Arrumando o Header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,7 +1,39 @@
 export default function Header () {
     return (
         <header>
-            <h1>Cabeçalho</h1>
+            <div className="main-cabecalho">
+                <div className="submain-cabecalho">
+                    <img src="./img/logo-starplus.png" className="logo-cabecalho"/>
+                    <div className="main-logos-cabecalho">
+                        <img src="./img/logo-inicio.png" className="minilogos-cabecalho"></img>
+                        <h1 className="texto-minilogos-cabecalho">INICIO</h1>
+                    </div>
+                    <div className="main-logos-cabecalho">
+                        <img src="./img/logo-lupa.png" className="minilogos-cabecalho"></img>
+                        <h1 className="texto-minilogos-cabecalho">PESQUISA</h1>
+                    </div>
+                    <div className="main-logos-cabecalho">
+                        <img src="./img/logo-mais.png" className="minilogos-cabecalho"></img>
+                        <h1 className="texto-minilogos-cabecalho">MINHA LISTA</h1>
+                    </div>
+                    <div className="main-logos-cabecalho">
+                        <img src="./img/logo-espn.png" className="minilogos-cabecalho"></img>
+                        <h1 className="texto-minilogos-cabecalho">ESPN</h1>
+                    </div>
+                    <div className="main-logos-cabecalho">
+                        <img src="./img/logo-filme.png" className="minilogos-cabecalho"></img>
+                        <h1 className="texto-minilogos-cabecalho">FILMES</h1>
+                    </div>
+                    <div className="main-logos-cabecalho">
+                        <img src="./img/logo-series.png" className="minilogos-cabecalho"></img>
+                        <h1 className="texto-minilogos-cabecalho">SERIES</h1>
+                    </div>
+                 </div>
+                <div className="submain-cabecalho">
+                    <p className="user-cabecalho">Pedro</p>
+                    <img className="foto-perfil-cabecalho" src="./img/foto-perfil.jpeg"></img>
+                </div>
+            </div>
         </header>
     )
 }
