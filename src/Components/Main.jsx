4cb9419560364f2a4a3efd04f93aa65e4da2f434
shimuzinho <pedro.moraes2@estudante.ifms.edu.br>
2024-04-05 19:55:19 -0300

    erros

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -5,16 +5,6 @@ export default function Main () {
   const [nome, setNome] = useState("")
   const [telefone, setTelefone] = useState(0)
   const [cpf, setCpf] = useState(0)
-<<<<<<< HEAD
-  const [listaContatos, setListaContatos] = useState([])
-
-  const registrar = (event) => {
-    event.preventDefault()
-    setListaContatos([...listaContatos, {
-      nomeSalvo: nome,
-      telefoneSalvo: telefone,
-      cpfSalvo: cpf,
-=======
   const [listaTelefone, setListaTelefone] = useState([])
 
   const registrar = (event) => {
@@ -23,40 +13,10 @@ export default function Main () {
       nomeSalvo: nome,
       telefoneSalvo: telefone,
       cpfSalvo: cpf
->>>>>>> bcf42ca12d36807be9b2fcc65592004d7a914ce5
     }])
   }
 
   return (
-<<<<<<< HEAD
-    <form action="" onSubmit={registrar}>
-      <label htmlFor="nome">
-        <input
-          type="text"
-          name="lista-telefone"
-          id="nome"
-          onChange={(event)=> setNome(event.target.value)}
-        />
-      </label>
-      <label htmlFor="telefone">
-        <input
-          type="tel"
-          name="lista-telefone"
-          id="telefone"
-          onChange={(event)=> setTelefone(event.target.value)}
-        />
-      </label>
-      <label htmlFor="cpf">
-        <input
-          type="number"
-          name="lista-telefone"
-          id="cpf"
-          onChange={(event)=> setCpf(event.target.value)}
-        />
-      </label>
-      <button>Cadastrar</button>
-    </form>
-=======
     <main>
       <form action="" onSubmit={registrar}>
         <label htmlFor="nome">
@@ -92,6 +52,5 @@ export default function Main () {
         <p>{contato.cpfSalvo}</p>
       </div>)}
     </main>
->>>>>>> bcf42ca12d36807be9b2fcc65592004d7a914ce5
   )
 }
