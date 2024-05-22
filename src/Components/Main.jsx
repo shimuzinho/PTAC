123a0a7a56811ffd8d2323a92b4150abf495d3d3
shimuzinho <pedro.moraes2@estudante.ifms.edu.br>
2024-05-22 06:59:11 -0300

    Comentarios

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -16,9 +16,9 @@ export default function Main () {
     }])
   }
 
-  const remover = (index) => {
-    let novaLista = listaTelefone.filter((el, indice) => (index != indice))
-    setListaTelefone(novaLista)
+  const remover = (index) => { // Cria a funcao remover e coloca nela um parametro, que poderia ser qualquer coisa, esssa funcao recebera um numero (um indice)
+    let novaLista = listaTelefone.filter((el, indice) => (index != indice)) // Cria uma nova lista usando o filter removendo o elemento do indice passado no parametro, usamos dois parametros no filter, pq o primeiro e o elemento e o segundo o indice
+    setListaTelefone(novaLista) // Setamos a nova lista, caso nao fizessemos isso, apenas iria remover o elemento mas nao apresentar ele removido
   }
 
   return (
@@ -50,15 +50,15 @@ export default function Main () {
         </label>
         <button>Cadastrar</button>
       </form>
-      {listaTelefone.map((contato, index) => 
-      <div key={index} className="listaContatos">
+      {listaTelefone.map((contato, index) => //Primeiro parametro o elemento o segundo o indice
+      <div key={index} className="listaContatos"> 
         <p>{contato.nomeSalvo}</p>
         <p>{contato.telefoneSalvo}</p>
         <p>{contato.cpfSalvo}</p>
         <button onClick={function () {
-          remover(index)
+          remover(index) // Crio um botao com uma funcao fantasma, que chama minha funcao com o parametro indice, esse botao e o que aparece como remover, precisa da funcao fantasma para passar um parametro
         }
-      }></button>
+      }>Remover</button>
       </div>)}
     </main>
   )
